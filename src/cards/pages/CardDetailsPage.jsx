b38881c14d2503010a28fcfc61b6cbf85e0c5b9c
ARcodedeveloper <132125566+ARcodedeveloper@users.noreply.github.com>
2023-07-31 23:29:29 +0300

    Update CardDetailsPage.jsx

--- src/cards/pages/CardDetailsPage.jsx
+++ src/cards/pages/CardDetailsPage.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import { useParams } from "react-router-dom";
 import { Client } from "@googlemaps/google-maps-services-js";
+import { useTheme } from '@mui/material/styles';
+
 
 const client = new Client({});
 
 export default function CardDetailsPage() {
   const [location, setLocation] = useState({ lat: 0, lng: 0 });
+  const [data, setData] = useState(null); 
   const { id } = useParams();
   const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyBfqePbYJs_djL3I_Xb_8FBxbhoVTWbjKs", 
+    googleMapsApiKey: "AIzaSyBfqePbYJs_djL3I_Xb_8FBxbhoVTWbjKs",
   });
 
   useEffect(() => {
@@ -17,11 +20,12 @@ export default function CardDetailsPage() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        setData(data); 
         client
           .geocode({
             params: {
-              address: `${data.address.street} ${data.address.houseNumber}, ${data.address.city}, ${data.address.country}`, 
-              key: "AIzaSyBfqePbYJs_djL3I_Xb_8FBxbhoVTWbjKs", 
+              address: `${data.address.street} ${data.address.houseNumber}, ${data.address.city}, ${data.address.country}`,
+              key: "AIzaSyBfqePbYJs_djL3I_Xb_8FBxbhoVTWbjKs",
             },
           })
           .then((r) => {
@@ -38,23 +42,42 @@ export default function CardDetailsPage() {
       })
       .catch((error) => console.log(error));
   }, [id]);
+  const theme = useTheme();
 
   if (loadError) return "map is not loaded";
   if (!isLoaded) return "loading map";
+  if (!data) return "loading data";
+
+  
+
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}> 
-    <GoogleMap 
-      id="marker-example"
-      mapContainerStyle={{
-        height: "60vh",
-        width: "60vw",
-      }}
-      zoom={14}
-      center={location}
-    >
-      <Marker position={location} />
-    </GoogleMap>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', position: 'relative' }}>
+      <GoogleMap
+        id="marker-example"
+        mapContainerStyle={{
+          height: "60vh",
+          width: "60vw",
+        }}
+        zoom={14}
+        center={location}
+      >
+        <Marker position={location} />
+      </GoogleMap>
+      <div style={{ 
+      color: theme.typography.h2.color, 
+      color: theme.typography.h3.color, 
+      color: theme.typography.h4.color,  
+      position: 'absolute', top: 10, left: 10 }}>
+        <h2>Card Details</h2>
+        <h3>{data.title}</h3>
+        <h3>{data.subtitle}</h3>
+        <h4>{data.description}</h4>
+        <h4>{data.address.country}</h4>
+        <h4>{data.address.city}</h4>
+        <h4>{data.address.street}</h4>
+        <h4>{data.address.houseNumber}</h4>
+        <h4>{data.address.postalCode}</h4>
+      </div>
     </div>
   );
 }
-
